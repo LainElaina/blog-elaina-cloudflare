0fd689fc0a0bb2146560e5f65937870c25ca5dce
LainElaina <1796137367@qq.com>
2026-03-23 16:31:06 +0000

    修复日志窗口导入路径

--- src/app/(home)/log-window.tsx
+++ src/app/(home)/log-window.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useLogStore } from '../stores/log-store'
+import { useLogStore } from './stores/log-store'
 import { toast } from 'sonner'
 
 export function LogWindow() {
